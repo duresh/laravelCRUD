1fed68d0edca38d65fc54460be750576922e8bd1
D. K. K. Weerasekara <ICT-ICTO019@core.statistics.gov.lk>
2022-09-15 15:17:18 +0530

    V 2.0

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Laravel 8|7|6 CRUD App Example</title>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+   </head>
+   <body>
+      <div class="container-fluid">
+      <nav class="navbar navbar-light navbar-expand-lg mb-5" style="background-color: #e3f2fd;">
+        <div class="container">
+            <a class="navbar-brand mr-auto" href="#">PositronX</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register-user') }}">Register</a>
+                    </li>
+                    @else
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('signout') }}">Logout</a>
+                    </li>
+                    @endguest
+                </ul>
+            </div>
+        </div>
+    </nav>
+      </div>
+      <div class="container">
+         
+         @yield('content')
+      </div>
+ 
+      <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" type="text/js"></script>
+   </body>
+</html>
